ad953989b74ba5f90200f0a882a551722ba7dade
Picasso <urvishp13@gmail.com>
2023-08-13 19:54:02 -0400

    got POSTER INFO section to display through js

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,1 +1,37 @@
+import { posts } from "./posts.js"
 
+// get original post layout element
+// make a copy of the original post layout
+    // make changes here
+// add the copy to the DOM
+
+let main = document.getElementsByTagName("main")[0]
+const postLayout = document.getElementById("post-layout")
+
+const posterInfo = document.getElementsByClassName("poster-info")[0]
+
+for (const obj of posts) {
+
+    const { name, username, location, avatar, post, comment, likes } = obj
+
+    getPosterInfo(name, avatar, location)
+}
+
+function getPosterInfo(name, avatar, location) {
+    const avatarEl = posterInfo.getElementsByClassName("avatar")[0]
+    const nameEl = posterInfo.getElementsByClassName("poster-name")[0]
+    const locationEl = posterInfo.getElementsByClassName("poster-location")[0]
+
+    const html = `
+                <img 
+                    src="assets/${avatar}" 
+                    alt="self-portrait of ${name}"
+                    class="avatar">
+                `
+
+    avatarEl.innerHTML = html
+    nameEl.textContent = `${name}`
+    locationEl.textContent = `${location}`
+
+    main.innerHTML += postLayout.outerHTML
+}
